037a607323050ab421942c8c3a871f534bc8f3cf
cactus <neel0108p@gmail.com>
2024-03-15 00:06:49 -0600

    Updated Swagger configuration

--- src/main/java/com/verdant/weather/service/controller/SearchTrackerController.java
+++ src/main/java/com/verdant/weather/service/controller/SearchTrackerController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name = "City Search Analytics", description = "APIs to Get Analytics On City Search Data.")
 @RestController
 @RequestMapping("api/v1/tracker")
 public class SearchTrackerController {
